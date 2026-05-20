80de7b06a17b228015854fc1dfe1ac5238399d58
Shailesh <shaileshkumaar4455@gmail.com>
2026-05-20 17:22:41 +0530

    production backend fix

--- server/index.js
+++ server/index.js
@@ -1,53 +1,73 @@
 require("dotenv").config();
+
 const express = require("express");
+
 const cors = require("cors");
 
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
+
 const stockRoutes = require("./routes/stockRoutes");
+
 const watchlistRoutes = require("./routes/watchlistRoutes");
+
 const alertRoutes = require("./routes/alertRoutes");
-const startAlertEngine = require("./utils/alertEngine");
-const notificationRoutes = require("./routes/notificationRoutes");
-const newsRoutes = require(
 
-  "./routes/newsRoutes"
+const notificationRoutes =
+  require("./routes/notificationRoutes");
 
-);
+const newsRoutes =
+  require("./routes/newsRoutes");
+
+const startAlertEngine =
+  require("./utils/alertEngine");
+
 const app = express();
+
 app.use(express.json());
+
 app.use(cors({
 
   origin:
-    "https://your-frontend.vercel.app",
+    "https://stockpulse-mu-three.vercel.app",
 
   credentials: true
 
 }));
 
 connectDB();
+
 startAlertEngine();
 
-const PORT = 5000;
+const PORT =
+  process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
+
 app.use("/api/stocks", stockRoutes);
+
 app.use("/api/watchlist", watchlistRoutes);
+
 app.use("/api/alerts", alertRoutes);
-app.use("/api/notifications", notificationRoutes);
+
 app.use(
-
-  "/api/news",
-
-  newsRoutes
-
+  "/api/notifications",
+  notificationRoutes
 );
 
+app.use("/api/news", newsRoutes);
+
 app.get("/", (req, res) => {
+
   res.send("Backend Server Running");
+
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
 });
